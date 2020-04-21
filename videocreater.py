63363617fe1250c6bd8ffa35a439514c7b5e69b3
enmoment <0xdjinn2017@gmail.com>
2020-04-21 23:01:43 +0800

    修改视频截取

--- videocreater.py
+++ videocreater.py
@@ -6,21 +6,24 @@ import dbmgr
 def create(jobid, contents_json):
     contents = json.loads(contents_json)
     clips = []
-    i = 0
+    i = 0 #尺寸初始化标志
     size = (0, 0)
     for content in contents:
         respath = dbmgr.get_res_path(content['mediaid'])
         duration = content['duration']
-        if content['mediatype'] == 'pic':
-            imgclip = ImageClip(img=respath, duration=duration)
+        #clip = None
+        mediatype = content['mediatype']
+        if  mediatype == 'pic':
+            clip = getpicClip(size=size,respath=respath,duration=duration)
 
-            if i == 0:
-                size = imgclip.size
-                clips.append(imgclip.fadeout(0.5, (1, 1, 1)))
-            else:
-                clips.append(imgclip.resize(size).fadeout(0.5, (1, 1, 1)).fadein(1, (1, 1, 1)))
+        elif mediatype == 'video':
+            clip = getvideoClip(size=size,respath=respath,duration=duration)
 
-            i += 1
+        if i == 0:
+            size = clip[0].size
+        clips.extend(clip)
+
+        i += 1
     finalclip = concatenate_videoclips(clips)
     filename = "%s.mp4" % str(jobid)
 
@@ -28,3 +31,26 @@ def create(jobid, contents_json):
     dbmgr.update_job_status(jobid=jobid, status='1', filename=filename)
 
 # create(1, contentstr)
+def getpicClip(size,respath,duration):
+    clips = []
+    clip = ImageClip(img=respath, duration=duration)
+    clips.append(clipTrans(size=size,clip=clip))
+    return clips
+def getvideoClip(size,respath,duration):
+    clips = []
+    clip = VideoFileClip(filename=respath)
+    if duration != 0:
+        #按duration截取视频
+        timestamps = duration.split('|')
+        for time in timestamps:
+            sub = time.split(',')
+            clips.append(clipTrans(size,clip.subclip(float(sub[0]),float(sub[1]))))
+    else:
+        clips.append(clipTrans(size=size,clip=clip))
+
+    return clips
+def clipTrans(size,clip):
+    if size != (0, 0):
+        return clip.resize(size).fadeout(0.5, (1, 1, 1)).fadein(1, (1, 1, 1))
+    else:
+        return clip.fadeout(0.5, (1, 1, 1))
